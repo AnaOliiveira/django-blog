502b04cd211b6bd76d7db8892064058a8157685d
Ana Oliveira <oliveira.miranda2102@gmail.com>
2023-06-28 11:31:32 -0300

    Introdução aos templates Django

---
+++ blog/views.py
@@ -0,0 +1,11 @@
+from django.shortcuts import render
+
+from django.http import HttpResponse
+from django.template.response import TemplateResponse
+
+# Create your views here.
+def index(request):
+    return render(request, 'index.html', {'titulo': 'Últimos Artigos'})
+
+def ola(request):
+    return HttpResponse('Olá, Django')
